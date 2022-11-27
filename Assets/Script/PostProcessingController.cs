0a3a48f4d9dc3145dbd017b58c2b4f3e0f6da6b7
Skye Mourad <Mourad.rea@gmail.com>
2022-11-27 14:19:56 -0500

    Fixed post processing on/off

--- Assets/Script/PostProcessingController.cs
+++ Assets/Script/PostProcessingController.cs
@@ -2,14 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PostProcessingController : MonoBehaviour
 {
     public static bool postProcessing = true;
+    [SerializeField] Toggle toggle;
+    private void Start()
+    {
+        toggle.isOn = postProcessing;
+    }
 
     public void PostProcessingOnOff(bool value)
     {
-        postProcessing = value;
+        postProcessing = toggle.isOn;
     }
 
     public void onBackButton()
